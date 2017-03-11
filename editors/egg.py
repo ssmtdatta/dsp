6b13d752d711d952849dcc7d5a8f4f26e23d9ebb
Susmita Datta <ssmtdatta@gmail.com>
2017-03-11 12:39:07 -0600

    created using emacs

---
+++ editors/egg.py
@@ -0,0 +1,10 @@
+"""
+Reataurents near me with the word 'egg' in their names
+"""
+
+restaurants = ["The Eastman Egg Company",
+               "Eggs, Inc. Cafe",
+               "West Egg Cafe"]
+
+for item in restaurants:
+    print(item)
---
+++ editors/vinegar.py
@@ -0,0 +1,8 @@
+print("The painful thing about vim is that I need to change to different edit and save modes before I can do anything.")
+
+modes = {}
+modes['insert'] = ":i"
+modes['write_quit'] = ":wq"
+
+print(modes)
+
