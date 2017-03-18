3a34dc5d469f494517433356ec4cef5e2cd12ac5
Susmita Datta <ssmtdatta@gmail.com>
2017-03-18 17:38:20 -0500

    code completed

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,24 @@
-PLACE YOUR CODE HERE
+# imports
+
+import csv
+
+input_filename = "faculty.csv"
+output_filename = "emails.csv"
+
+ff = open(output_filename, 'w')
+
+with open(input_filename) as f:
+    csv_file = csv.reader(f)
+    headers = next(csv_file)
+    
+    ff.write("email\n")
+
+    for line in csv_file:
+    	*fields, email = line
+    	ff.write(email)
+    	ff.write("\n")
+       
+f.close()
+ff.close()
+
+
