45066511a328b5f42b7cef125126334c7693987b
Susmita Datta <ssmtdatta@gmail.com>
2017-03-23 19:42:05 -0500

    cleaned up code

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -24,10 +24,7 @@ between two date objects.
 
 
 from datetime import datetime as dt 
-
-# date formats
-date_format_1 = "%m-%d-%Y" 
-date_format_2 = "%d-%b-%Y" 
+import calendar
 
 def countDays(date_start, date_stop, date_format):
 	"""
@@ -40,37 +37,37 @@ def countDays(date_start, date_stop, date_format):
 
 def formatDate(a_date):
 	'''
-	Convert to date_format_1 as described at the top
+	date_format_1 = "%m-%d-%Y" 
+	date_format_2 = "%d-%b-%Y" 
+	date_format_3 = "%m%d%Y" 
+ 	Convert to format 1 as described at the top
+ 	Convert to date_format_1 as described at the top
 	'''
 	m = a_date[:2]
 	d = a_date[2:4]
 	y = a_date[-4:]
-	return str(m)+'-'+str(d)+'-'+str(y)
+	return str(m)+'-'+str(calendar.month_abbr[int(m)])+'-'+str(y)
 
 
-
-####a) 
+####a) date format: month-day-year
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'
 
-days_in_between = countDays(date_start, date_stop, date_format_1)
+days_in_between = countDays(date_start, date_stop, "%m-%d-%Y")
 print("There are "+str(days_in_between)+" days between "+str(date_start)+" and "+str(date_stop)+".")
  
 
-####b)  
+####b) date format: MonthDayYear
 date_start = '12312013'  
 date_stop = '05282015' 
 
-date_start = formatDate(date_start)
-date_stop = formatDate(date_stop)
-
-days_in_between = countDays(date_start, date_stop, date_format_1)
-print("There are "+str(days_in_between)+" days between "+str(date_start)+" and "+str(date_stop)+".")
+days_in_between = countDays(date_start, date_stop, "%m%d%Y" )
+print("There are "+str(days_in_between)+" days between "+str(formatDate(date_start))+" and "+str(formatDate(date_stop))+".")
 
 
-####c)  
+####c) date format: day-month-year
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
 
-days_in_between = countDays(date_start, date_stop, date_format_2)
+days_in_between = countDays(date_start, date_stop, "%d-%b-%Y")
 print("There are "+str(days_in_between)+" days between "+str(date_start)+" and "+str(date_stop)+".")
